7f34590b1ee0d8eccf1850ff96fc083e21e702c9
Pablo Morales <33302229+pmoralesgarcia@users.noreply.github.com>
2023-04-13 10:54:11 -0700

    Create indieauth.php

---
+++ indieauth.php
@@ -0,0 +1,11 @@
+<?php
+// Indieauth extension, https://github.com/pmoralesgarcia/indieauth-datenstrom
+
+class YellowEdit {
+    const VERSION = "0.8.71";
+    public $yellow;         // access to API
+    public $response;       // web response
+    public $merge;          // text merge
+    public $editable;       // page can be edited? (boolean)
+    
+?>
